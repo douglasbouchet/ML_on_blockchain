8c7bfec4fac4f82a4a3ad1401c506529446edd93
douglas <douglas.bouchet@epfl.ch>
2022-10-22 08:00:11 +0000

    reading account value from blockchain

--- script/deploy-smart-contract.py
+++ script/deploy-smart-contract.py
@@ -1,4 +1,22 @@
-from web3 import Web3, EthereumTesterProvider
+from web3 import Web3
 
-w3 = Web3(EthereumTesterProvider())
-print(w3.isConnected())
+# connect to one of the local quorum blockchain node
+web3 = Web3(Web3.WebsocketProvider("ws://192.168.203.3:9000"))
+web3 = Web3(Web3.WebsocketProvider("ws://192.168.203.4:9000"))
+print(web3.isConnected())
+
+
+# print(w3.eth.get_block("latest"))
+
+print(web3.eth.accounts)
+
+print(web3.eth.protocol_version)
+
+
+print(web3.toChecksumAddress("0xfce75e885241b4b465ad8e5919416ad4c9290d3e"))
+# print(web3.eth.get_balance("0xfce75e885241b4b465ad8e5919416ad4c9290d3e"))
+print(
+    web3.eth.get_balance(
+        web3.toChecksumAddress("0xfce75e885241b4b465ad8e5919416ad4c9290d3e")
+    )
+)
