260f84d29599eb80e547430b01417e591fe3ac09
douglas <douglas.bouchet@epfl.ch>
2022-10-23 19:54:53 +0000

    wip on global methods to generate smart contracts and deploy them

--- script/compile.py
+++ script/compile.py
@@ -1,20 +1,64 @@
 # 1. Import solcx
 import solcx
 
-# 2. If you haven't already installed the Solidity compiler, uncomment the following line
-# solcx.install_solc()
+# # 2. If you haven't already installed the Solidity compiler, uncomment the following line
+# # solcx.install_solc()
 
-# 3. Compile contract
-# temp_file = solcx.compile_files('../smart-contracts/Incrementer.sol')
-temp_file = solcx.compile_files(
-    "/home/user/ml-on-blockchain/smart-contracts/incrementer.sol"
-)
+# # 3. Compile contract
+# # temp_file = solcx.compile_files('../smart-contracts/Incrementer.sol')
+# temp_file = solcx.compile_files(
+#     "/home/user/ml-on-blockchain/smart-contracts/incrementer.sol"
+# )
 
-# 4. Export contract data
-# abi = temp_file['Incrementer.sol:Incrementer']['abi']
-abi = temp_file[
-    "/home/user/ml-on-blockchain/smart-contracts/incrementer.sol:Incrementer"
-]["abi"]
-bytecode = temp_file[
-    "/home/user/ml-on-blockchain/smart-contracts/incrementer.sol:Incrementer"
-]["bin"]
+# # 4. Export contract data
+# # abi = temp_file['Incrementer.sol:Incrementer']['abi']
+# abi = temp_file[
+#     "/home/user/ml-on-blockchain/smart-contracts/incrementer.sol:Incrementer"
+# ]["abi"]
+# bytecode = temp_file[
+#     "/home/user/ml-on-blockchain/smart-contracts/incrementer.sol:Incrementer"
+# ]["bin"]
+
+
+def compile_smart_contract(contract_name):
+    """Compile smart contract using solcx.
+
+    Args:
+        contract_name (str): Name of the contract to compile.
+
+    Returns:
+        dict: Compiled smart contract.
+    """
+    temp_file = solcx.compile_files(
+        f"/home/user/ml-on-blockchain/smart-contracts/{contract_name}.sol"
+    )
+
+
+def get_abi(contract_name):
+    """Get ABI of smart contract.
+    Require to compile smart contract first.
+
+    Args:
+        contract_name (str): Name of the contract to compile.
+
+    Returns:
+        dict: ABI of smart contract.
+    """
+    return temp_file[
+        f"/home/user/ml-on-blockchain/smart-contracts/{contract_name}.sol:{contract_name}"
+    ]["abi"]
+
+
+def get_bytecode(contract_name):
+    """Get bytecode of smart contract.
+    Require to compile smart contract first.
+
+    Args:
+        contract_name (str): Name of the contract to compile.
+
+    Returns:
+        dict: Bytecode of smart contract.
+    """
+    return temp_file[
+        f"/home/user/ml-on-blockchain/smart-contracts/{contract_name}.sol:{contract_name}"
+    ]["bin"]
--- script/deploy.py
+++ script/deploy.py
@@ -1,6 +1,7 @@
 # 1. Import the ABI and bytecode
 from web3 import Web3
 from compile import abi, bytecode
+from compile import compile_smart_contract, get_abi, get_bytecode
 
 # 2. Add the Web3 provider logic here:
 web3 = Web3(Web3.WebsocketProvider("ws://192.168.203.3:9000"))
@@ -38,3 +39,42 @@ tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
 tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
 print(f"Contract deployed at address: { tx_receipt.contractAddress }")
+
+
+# def deploy_smart_contract(contract_name, from_address, from_private_key):
+#     # 2. Add the Web3 provider logic here:
+#     web3 = Web3(Web3.WebsocketProvider("ws://192.168.203.3:9000"))
+#     print("web3 connected: {}".format(web3.isConnected()))
+
+#     # 3. Create address variable
+#     account_from = {
+#         "private_key": "bde4a6df57a58a186ed2099754877a16bd48a10bc4469377d9840f475772723d",
+#         "address": "fce75e885241b4b465ad8e5919416ad4c9290d3e",
+#     }
+
+#     print(f'Attempting to deploy from account: { account_from["address"] }')
+
+#     # 4. Create contract instance
+#     Incrementer = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+#     # 5. Build constructor tx
+#     construct_txn = Incrementer.constructor(5).buildTransaction(
+#         {
+#             "gasPrice": 0,
+#             "from": Web3.toChecksumAddress(account_from["address"]),
+#             "nonce": web3.eth.get_transaction_count(
+#                 Web3.toChecksumAddress(account_from["address"])
+#             ),
+#         }
+#     )
+
+#     # 6. Sign tx with PK
+#     tx_create = web3.eth.account.sign_transaction(
+#         construct_txn, account_from["private_key"]
+#     )
+
+#     # 7. Send tx and wait for receipt
+#     tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
+#     tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+
+#     print(f"Contract deployed at address: { tx_receipt.contractAddress }")
