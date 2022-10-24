dd1ac43610bfc20b400ebd163d51cf0ebea3ab2c
douglas <douglas.bouchet@epfl.ch>
2022-10-24 08:00:25 +0000

    app to deploy smart contract and increase its value

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from src.basic_server import BasicServer
+from src.basic_worker import BasicWorker
 
 
 def init_server():
@@ -10,9 +11,25 @@ def init_server():
     return basic_server
 
 
+def create_single_worker():
+    worker_address = "ad8c4637330e8eab5eadbbda59910a9d926274b2"
+    worker_private_key = (
+        "9793a9cb6042ef94219797af47062b38100e535fdb7034a2ae9ba4136a6d17b4"
+    )
+    basic_worker = BasicWorker(worker_address, worker_private_key)
+    return basic_worker
+
+
 def main():
+    # ------Init server and worker--------
     basic_server = init_server()
-    contract_address = basic_server.deploy()
+    basic_worker = create_single_worker()
+    # ------Deploy smart contract---------
+    contract = basic_server.deploy()
+    print("Initial number of workers:", contract.get_number_of_workers())
+    # ------Register worker to server-----
+    basic_worker.register_to_learning(contract.contract_address, contract.abi)
+    print("current number of workers:", contract.get_number_of_workers())
 
 
 if __name__ == "__main__":
