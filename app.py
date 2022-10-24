03c6854a3e467e640574722b60d976f690617c85
douglas <douglas.bouchet@epfl.ch>
2022-10-24 07:24:40 +0000

    basic server class

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from src.basic_server import main
+from src.basic_server import BasicServer
 
-main()
+
+def init_server():
+    server_address = "fce75e885241b4b465ad8e5919416ad4c9290d3e"
+    server_private_key = (
+        "bde4a6df57a58a186ed2099754877a16bd48a10bc4469377d9840f475772723d"
+    )
+    basic_server = BasicServer(server_address, server_private_key)
+    return basic_server
+
+
+def main():
+    basic_server = init_server()
+    contract_address = basic_server.deploy()
+
+
+if __name__ == "__main__":
+    main()
--- src/basic_server.py
+++ src/basic_server.py
@@ -1,6 +1,18 @@
 from .script.deploy import deploy_smart_contract
 
 
+class BasicServer:
+    def __init__(self, from_address, from_private_key):
+        self.from_address = from_address
+        self.from_private_key = from_private_key
+
+    def deploy(self):
+        contract_name = "incrementer"
+        contract_adress = deploy_smart_contract(
+            contract_name, self.from_address, self.from_private_key
+        )
+
+
 def main():
     contract_name = "incrementer"
     from_address = "fce75e885241b4b465ad8e5919416ad4c9290d3e"
