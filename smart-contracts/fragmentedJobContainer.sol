17fe964457c35dcea9b8b80a8dde0a61050267a9
Douglas Bouchet <douglas.bouchet@epfl.ch>
2022-11-13 10:55:50 +0000

    implemented fragmented job smart contract, needs test

--- smart-contracts/fragmentedJobContainer.sol
+++ smart-contracts/fragmentedJobContainer.sol
@@ -2,22 +2,23 @@
 pragma solidity ^0.7.0;
 
 contract JobContainer {
-    //-------------------
+    struct Fragment {
+        uint256 fragNumber;
+        int256 weight;
+    }
+    //------------------- fragment attributs-------------------------------------------
     uint256 public nFragments; // the number of fragments under which our model is split
-    mapping(bytes32 => mapping(uint256 => int256)) public modelHashToFragments; // modelHash -> (fragNb -> weight)
-    bool modelIsReady = false; // true if a model has been merged and posses correct hash
-    //---------------------
-    uint16 nModelsUntilEnd;
+    bytes32[] public modelsHashes; // hashes of the models received sor far
+    mapping(bytes32 => Fragment[]) public modelHashToFragments; // modelHash -> [Fragment]
+    mapping(bytes32 => uint256) public modelHashToSize; // modelHash -> numbe of fragments
+    //------------------- workers attributs-------------------------------------------
+    mapping(address => bytes32) public workerAddressToModelHash;
+    address[] private receivedModelsAddresses; // each time a worker sends a model, it's address is added to this array
+    //------------------- model attributs-------------------------------------------
     uint256 batchIndex;
     int256 public currentModel;
-    //int256[] private receivedModels;
-    int256[] public receivedModels;
-    mapping(address => int256) public workerAddressToReceivedModels;
-    bool private jobFinished;
-    int256 public bestModel;
-    address[] private receivedModelsAddresses; // each time a worker sends a model, it's address is added to this array
-    mapping(int256 => uint256) private modelToCount; // count how many time each model weights were received
-    uint256 private bestModelNoccurences; // count how many time each model weights were received
+    int256 newModel; // the weight of the new model
+    bool modelIsReady = false; // true if a model has been merged and posses correct hash
 
     mapping(bytes32 => address) private modelHashToWorkerAddress; // usefull to pay worker that did provide correct models
 
@@ -35,82 +36,26 @@ contract JobContainer {
     }
 
     constructor(
-        uint16 _nModelsUntilEnd,
+        uint16 _nFragments,
         int256 _currentModel,
         uint256 _batchIndex
     ) {
-        nModelsUntilEnd = _nModelsUntilEnd;
+        nFragments = _nFragments;
         currentModel = _currentModel;
         batchIndex = _batchIndex;
-    }
-
-    function getNModelsUntilEnd() public view returns (uint16) {
-        return nModelsUntilEnd;
+        //TODO init all fragments to false
     }
 
     function getModelAndBatchIndex() public view returns (int256, uint256) {
         return (currentModel, batchIndex);
     }
 
-    function getBestModel() public view returns (int256) {
-        return bestModel;
-    }
-
-    function computeBestModel() private returns (bool) {
-        /** Group the received models, and keep the one with the most occurences
-         * TODO implement reverse if dominant model has less than majority
-         * @return the best model from receivedModels TODO null value if no consensus
-         */
-        for (uint256 i = 0; i < receivedModels.length; i++) {
-            if (modelToCount[receivedModels[i]] > bestModelNoccurences) {
-                bestModel = receivedModels[i];
-                bestModelNoccurences = modelToCount[receivedModels[i]];
-            }
-        }
-
-        // if less than 50% of workers agree on the same model, return null
-        // TODO implement this logic
-        // if (bestModelNoccurences < receivedModels.length / 2) {
-        // revert
-        //     return 0;
-        //return false
-        // }
-        return true;
-    }
-
-    function submitNewModel(int256 _model, address _workerAddress)
-        public
-        modelOnlySendOnce(_workerAddress)
-        returns (bool)
-    {
-        /** This method should only be called once by each worker, i.e you cannot submit same job multiple time
-         * @return true if the job is finished, false otherwise
-         */
-        // add the model to the list of received models
-        workerAddressToReceivedModels[_workerAddress] = _model;
-        receivedModels.push(_model);
-        // update modelToCount
-        modelToCount[_model] += 1;
-
-        // if we received enough models, we can compute the best model
-        if (receivedModels.length == nModelsUntilEnd) {
-            //bool foundBestModel = computeBestModel();
-            computeBestModel();
-            // add here handling if computeBestModel didn't found a consensus
-            jobFinished = true;
-
-            // we can now pay the workers that did provide the best model
-            payWorkers();
-        }
-        return jobFinished;
-    }
-
-    function payWorkers() private {
+    function payWorkers(bytes32 _modelHash) private {
         for (uint256 i = 0; i < receivedModelsAddresses.length; i++) {
             // if the model of worker is the best model, we pay the worker
             if (
-                workerAddressToReceivedModels[receivedModelsAddresses[i]] ==
-                bestModel
+                workerAddressToModelHash[receivedModelsAddresses[i]] ==
+                _modelHash
             ) {
                 //TODO compute this value such as expectation for good worker is > 0
                 // transfert money to worker
@@ -120,72 +65,100 @@ contract JobContainer {
     }
 
     // ----------- DEBUG FUNCTIONS -------------
-    function getReceivedModels() public view returns (int256[] memory) {
-        /** TODO this function is only for debug purpose, we will not allow people to get the current received worker */
-        return receivedModels;
-    }
-
-    // we need map: fragNb -> List (weights, Modelhash)
-
-    // ! function to add fragment (fragNb, weights, modelHash) -> bool -> if yes nthg, if no, worker needs send another frag
-
-    // ! function to tell if we have sufficient number of frags fragmentationCompleted() -> bool
-
-    // ! function to merge fragments mergeFragments() -> [weights, modelHash] trigger upon fragmentationCompleted
-
-    // ! function to check if model is valid checkModel([weights], modelHash) -> bool (check if hash([weights]) == modelHash)
 
     //------------ Fragment methods-----------------
 
     /// @notice Add a fragment of the model
+    /// @dev handle all the logic upon adding a new fragment, including merging the model if all fragments are received,
+    /// checking if the model is valid and paying the workers if the model is valid
     /// @param _fragNb identifier of the fragment
-    /// @param _weights weights of the model's fragment
+    /// @param _weight weights of the model's fragment
     /// @param _modelHash Hash of the model
-    /// @return True if we don't have any fragment with this number associated to this model hash. otw false, and the
+    /// @return True if we don't have any fragment with this number associated to this model hash. false otw and the
     /// worker needs to send another fragment
     function addFragment(
+        address _workerAddress,
         uint256 _fragNb,
         int256 _weight,
         bytes32 _modelHash
     ) public returns (bool) {
-        //TODO
-        return true;
-    }
+        // check if we have a model with this hash
+        bool _hashExists = false;
+        for (uint256 i = 0; i < modelsHashes.length; i++) {
+            if (modelsHashes[i] == _modelHash) {
+                _hashExists = true;
+            }
+        }
+        // if we don't have a model with this hash, we add it to modelsHashes
+        if (!_hashExists) {
+            modelsHashes.push(_modelHash);
+            modelHashToSize[_modelHash] = 0;
+            modelHashToFragments[_modelHash] = new Fragment[](nFragments); //TODO see if correctly created
+        }
 
-    /// @dev function to tell if we have sufficient number of frags
-    /// @return True if a model hash has enough fragments, false otherwise. If true, we can merge the fragments
-    function fragmentationCompleted() private view returns (bool) {
-        //TODO
+        uint256 _nFrags = modelHashToSize[_modelHash];
+        // now we iterate over all the fragments of the model hash and see if we already have a fragment with this number
+        for (uint256 i = 0; i < _nFrags; i++) {
+            if (modelHashToFragments[_modelHash][i].fragNumber == _fragNb) {
+                // we already have a fragment with this number
+                return false;
+            }
+        }
+
+        // now we know we don't have received this fragment yet, we can add it to the model
+
+        // update the number of fragments for this model hash
+        modelHashToSize[_modelHash] = _nFrags + 1;
+
+        // add the fragment to the list of fragments
+        modelHashToFragments[_modelHash][_nFrags] = Fragment(_nFrags, _weight);
+
+        // add the worker to the list of workers that sent fragments for this model hash
+        workerAddressToModelHash[_workerAddress] = _modelHash;
+
+        // if we have enough fragments, we can merge them
+        if (_nFrags + 1 == nFragments) {
+            bool correctModel = mergeFragments(_modelHash);
+            if (correctModel) {
+                // we can now pay the workers that did provide the best model
+                payWorkers(_modelHash);
+            }
+            // TODO handle case where model is not correct
+        }
+
         return true;
     }
 
     /// @dev function to merge fragments
-    /// @dev the model hash is computede as the hash of the combined weights
-    /// @return [weights, modelHash] of the model
-    function mergeFragments() private returns (int256[] memory) {
-        //TODO
-        return new int256[](0);
-    }
+    /// @dev the model hash is computed as the hash of the combined weights
+    /// @dev if the computed model hash is the same as the expected one, set the new model
+    /// @param _modelHash of the model to merge inside modelHashToFragments
+    /// @return true if the model is valid, false otherwise
+    function mergeFragments(bytes32 _modelHash) private returns (bool) {
+        // get the model weights into one array
+        int256[] memory weights = new int256[](nFragments);
+        for (uint256 i = 0; i < nFragments; i++) {
+            weights[i] = modelHashToFragments[_modelHash][i].weight;
+        }
 
-    /// @dev function to check if model is valid
-    /// @dev trivial, so maybe don't need it
-    /// @param _weights weights of the model
-    /// @param _modelHash Hash of the model
-    /// @return True if the model is valid, false otherwise
-    function checkModel(int256[] memory _weights, bytes32 _modelHash)
-        private
-        pure
-        returns (bool)
-    {
-        //TODO
-        return true;
+        // compute the model hash
+        bytes32 _computedModelHash = keccak256(abi.encode(weights));
+
+        // if the computed model hash is the same as the expected one, set the new model
+        newModel = weights[0]; // Only first weight as we consider model as a single int256, but we will need to change this
+        modelIsReady = true;
+
+        // check if the computed model hash is the same as the expected one
+        return _computedModelHash == _modelHash;
     }
 
     /// @notice function to get the model
-    /// @dev if the model is not valid, return null
-    /// @return [weights, modelHash] of the model
-    function getModel() public returns (int256[] memory) {
-        //TODO
-        return new int256[](0);
+    /// @return the model's weights or empty array along with a boolean indicating if the model is valid
+    function getModel() public view returns (int256, bool) {
+        if (modelIsReady) {
+            return (newModel, true);
+        } else {
+            return (0, false);
+        }
     }
 }
