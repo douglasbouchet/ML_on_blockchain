e1d5cb57accb75ae40b51de45b9e1272087ee975
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-01-02 18:16:41 +0100

    saving output of primary in local machine

--- script/dcl_script/multiple_test.sh
+++ script/dcl_script/multiple_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Read in a string from the user
-read -p "Enter a string: " str
+read -p "Enter a string: " scenario
 
-echo "Scenario: $str"
+echo "Scenario: $scenario"
 
 num_workers=1
 
@@ -11,6 +11,9 @@ num_workers=1
 for i in {1..1}; do
     # Multiply the variable by 10
     num_workers=$((num_workers * 10))
+    # create file names as number_of_workers and scenario (to save results)
+    file_name="$scenario"_"$num_workers"_workers
+    touch res/$file_name.txt
     # Print the variable
     echo "Testing blockchain with $num_workers workers"
     echo "Deploying blockchain..."
@@ -18,13 +21,15 @@ for i in {1..1}; do
     # timeout 35 ssh user@dclbigmem.epfl.ch -p 2232 "cd ~/minion && ./bin/minion run -vvv --user=user --breakpoint=chain quorum ../workloads/counter.yaml 127.0.0.1,192.168.203.3@vm-dclbigmem-1"
     echo "  deployed blockchain"
     # open 2 ssh connections to launch primary and secondary
-    #timeout 80 ssh user@dclbigmem.epfl.ch -p 2232 'echo $PATH; export PATH=$PATH:/home/user/solidity/build/solc/ ; echo $PATH; /home/user/diablo/diablo primary -vvv --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=workloads/solidity-contracts/ 1 deploy/diablo/primary/setup.yaml workloads/counter.yaml' &
-    ssh user@dclbigmem.epfl.ch -p 2232 'echo $PATH; export PATH=$PATH:/home/user/solidity/build/solc/ ; echo $PATH; /home/user/diablo/diablo primary -vvv --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=workloads/solidity-contracts/ 1 deploy/diablo/primary/setup.yaml workloads/counter.yaml' &
+    # ssh user@dclbigmem.epfl.ch -p 2232 'export PATH=$PATH:/home/user/solidity/build/solc/ ; /home/user/diablo/diablo primary -vvv --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=workloads/solidity-contracts/ 1 deploy/diablo/primary/setup.yaml workloads/counter.yaml' &
+    ssh user@dclbigmem.epfl.ch -p 2232 'export PATH=$PATH:/home/user/solidity/build/solc/ ; /home/user/diablo/diablo primary -vvv --output=out.txt --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=workloads/solidity-contracts/ 1 deploy/diablo/primary/setup.yaml workloads/counter.yaml; ' &
     # wait 5 seconds that primary is launched and then launch secondary
     sleep 5
-    #timeout 80 ssh user@dclbigmem.epfl.ch -p 2232 "/home/user/diablo/diablo secondary -vvv localhost" &
     ssh user@dclbigmem.epfl.ch -p 2232 "/home/user/diablo/diablo secondary -vvv localhost" &
     sleep 60
+    scp user@dclbigmem.epfl.ch:out.txt res/
+    # rename res/out.txt to res/$file_name.txt
+    mv res/out.txt res/$file_name.txt
     exit
 
 done
