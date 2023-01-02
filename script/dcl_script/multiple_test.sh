c210255c162a61f6799087ede0cedd95855bd2bd
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-01-02 17:42:13 +0100

    launching primary and secondary

--- script/dcl_script/multiple_test.sh
+++ script/dcl_script/multiple_test.sh
@@ -13,11 +13,18 @@ for i in {1..1}; do
     num_workers=$((num_workers * 10))
     # Print the variable
     echo "Testing blockchain with $num_workers workers"
-    # connect in ssh to launch the blockchain
-    # ssh user@dclbigmem.epfl.ch -p 2232 "cd ~/test && touch gg$i.txt"
-    # ssh user@dclbigmem.epfl.ch -p 2232 "cd ~/minion && ./bin/minion run -vvv --user=user --breakpoint=chain quorum ../workloads/counter.yaml 127.0.0.1,192.168.203.3,192.168.203.4,192.168.203.5,192.168.203.6@vm-dclbigmem-1"
     echo "Deploying blockchain..."
-    timeout 35 ssh user@dclbigmem.epfl.ch -p 2232 "cd ~/minion && ./bin/minion run -vvv --user=user --breakpoint=chain quorum ../workloads/counter.yaml 127.0.0.1,192.168.203.3@vm-dclbigmem-1"
-    echo "deployed blockchain"
+    # connect in ssh to launch the blockchain
+    # timeout 35 ssh user@dclbigmem.epfl.ch -p 2232 "cd ~/minion && ./bin/minion run -vvv --user=user --breakpoint=chain quorum ../workloads/counter.yaml 127.0.0.1,192.168.203.3@vm-dclbigmem-1"
+    echo "  deployed blockchain"
+    # open 2 ssh connections to launch primary and secondary
+    #timeout 80 ssh user@dclbigmem.epfl.ch -p 2232 'echo $PATH; export PATH=$PATH:/home/user/solidity/build/solc/ ; echo $PATH; /home/user/diablo/diablo primary -vvv --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=workloads/solidity-contracts/ 1 deploy/diablo/primary/setup.yaml workloads/counter.yaml' &
+    ssh user@dclbigmem.epfl.ch -p 2232 'echo $PATH; export PATH=$PATH:/home/user/solidity/build/solc/ ; echo $PATH; /home/user/diablo/diablo primary -vvv --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=workloads/solidity-contracts/ 1 deploy/diablo/primary/setup.yaml workloads/counter.yaml' &
+    # wait 5 seconds that primary is launched and then launch secondary
+    sleep 5
+    #timeout 80 ssh user@dclbigmem.epfl.ch -p 2232 "/home/user/diablo/diablo secondary -vvv localhost" &
+    ssh user@dclbigmem.epfl.ch -p 2232 "/home/user/diablo/diablo secondary -vvv localhost" &
+    sleep 60
+    exit
 
 done
