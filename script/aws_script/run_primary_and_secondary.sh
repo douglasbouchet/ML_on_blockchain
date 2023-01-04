32437f0a4992bca8f4cceaee4f9efb27bfae2734
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-01-04 13:51:08 +0100

    replacing user by ubuntu for vm connection

--- script/aws_script/run_primary_and_secondary.sh
+++ script/aws_script/run_primary_and_secondary.sh
@@ -20,16 +20,16 @@ echo "Number of secondary addresses: $num_secondary"
 
 # connect to primary and launch diablo primary
 # ssh user@dclbigmem.epfl.ch -p 2232 'export PATH=/home/user/.local/bin:/home/user/anaconda3/bin:/home/user/anaconda3/condabin:/home/user/solidity/build/solc:/home/user/diablo:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/user/solidity/build/solc/ ; /home/user/diablo/diablo primary -vvv --output=out.txt --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=ml_on_blockchain/smart-contracts/federatedLearning/ 1 deploy/diablo/primary/setup.yaml ml_on_blockchain/workload/federated_learning_generated.yaml; ' &
-ssh user@$1 'export PATH=install/solidity/build/solc/:$PATH; ./install/diablo/diablo primary --env=accounts=install/geth-accounts/accounts.yaml --env=contracts=ml_on_blockchain/smart-contracts/federatedLearning/ --port=3000 -vvv --stat '"$num_secondary"' setup.yaml workload.yaml; ' &
+ssh ubuntu@$1 'export PATH=install/solidity/build/solc/:$PATH; ./install/diablo/diablo primary --env=accounts=install/geth-accounts/accounts.yaml --env=contracts=ml_on_blockchain/smart-contracts/federatedLearning/ --port=3000 -vvv --stat '"$num_secondary"' setup.yaml workload.yaml; ' &
 # wait 5 seconds that primary is launched and then launch secondary
 sleep 5
 # connect to each secondary and launch diablo secondary
 for var in "${@:2}";do
-    ssh user@$var './install/diablo/diablo secondary -vvv --port=3000 --tag=any ' $1 &
+    ssh ubuntu@$var './install/diablo/diablo secondary -vvv --port=3000 --tag=any ' $1 &
 done
 wait
 # copy the result file from primary to local machine
-scp user@$1:out.txt res/
+scp ubuntu@$1:out.txt res/
 mv res/out.txt res/$scenario/$file_name.txt
 echo -e "\n"
 
