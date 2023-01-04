2461373fa1eb6dc374ca59db7166823c28203378
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-01-04 10:55:03 +0100

    adding script to generate workload in aws scripts

---
+++ script/aws_script/create_workload.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# this script expect the number of workers as argument
+n_workers=$1
+# we divide the number of workers by 10 to get number of call to smart contract to make per second
+n_calls_per_second=$((n_workers/10))
+echo "Number of workers: $n_workers"
+echo "Number of calls per second: $n_calls_per_second"
+
+cat <<EOF > workload.yaml
+let:
+  - !loop &account
+    sample: !account
+      number: 100
+      stake: 10000000
+  - !loop &any_location
+    sample: !location
+      - ".*"
+  - !loop &any_endpoint
+    sample: !endpoint
+      - ".*"
+  - &contract
+    sample: !contract
+      number: 1
+      name: "learn_task"
+workloads:
+  - number: 1
+    client:
+      location: *any_location
+      view: *any_endpoint
+      behavior:
+        - interaction: !invoke
+            from: *account
+            contract: *contract
+            function: "getModelAndBatchIndex()"
+          load:
+            0: $n_calls_per_second
+            10: 0
+        - interaction: !invoke
+            from: *account
+            contract: *contract
+            function: "addNewEncryptedModel()"
+          load:
+            0: $n_calls_per_second
+            10: 0
+        - interaction: !invoke
+            from: *account
+            contract: *contract
+            function: "addVerificationParameters()"
+          load:
+            10: $n_calls_per_second
+            20: 0
+EOF
+
+echo "Workload file created: workload.yaml"
