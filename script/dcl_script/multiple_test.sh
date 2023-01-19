4971d63e1f90ca51c2de0e5566d53349eacfb731
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-01-19 17:54:22 +0000

    not using federated_learning_generated.yaml

--- script/dcl_script/multiple_test.sh
+++ script/dcl_script/multiple_test.sh
@@ -26,7 +26,7 @@ for i in {1..7}; do
     # call the script create_workload.sh with the number of workers as argument
     ./create_workload.sh $num_workers
     # upload the workload.yaml file to the dclbigmem machine
-    scp generated/workload.yaml user@dclbigmem.epfl.ch:~/ml_on_blockchain/workload/federated_learning_generated.yaml
+    scp generated/workload.yaml user@dclbigmem.epfl.ch:~/ml_on_blockchain/workload/federated_learning.yaml
     # create the contract (as we modify the number of workers, we must update it inside the smart contract)
     ./create_smartcontract.sh $num_workers $redundancy
     # upload the contract.sol to the dclbigmem machine
@@ -37,7 +37,7 @@ for i in {1..7}; do
     timeout 35 ssh user@dclbigmem.epfl.ch -p 2232 "cd ~/minion && ./bin/minion run -vvv --user=user --breakpoint=chain quorum ../workloads/counter.yaml 127.0.0.1,192.168.203.3,192.168.203.4,192.168.203.5,192.168.203.6@vm-dclbigmem-1"
     echo "deployed blockchain"
     # open 2 ssh connections to launch primary and secondary
-    ssh user@dclbigmem.epfl.ch -p 2232 'export PATH=/home/user/.local/bin:/home/user/anaconda3/bin:/home/user/anaconda3/condabin:/home/user/solidity/build/solc:/home/user/diablo:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/user/solidity/build/solc/ ; /home/user/diablo/diablo primary -vvv --output=out.txt --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=ml_on_blockchain/smart-contracts/federatedLearning/ 1 deploy/diablo/primary/setup.yaml ml_on_blockchain/workload/federated_learning_generated.yaml; ' &
+    ssh user@dclbigmem.epfl.ch -p 2232 'export PATH=/home/user/.local/bin:/home/user/anaconda3/bin:/home/user/anaconda3/condabin:/home/user/solidity/build/solc:/home/user/diablo:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/user/solidity/build/solc/ ; /home/user/diablo/diablo primary -vvv --output=out.txt --stat --env=accounts=deploy/diablo/primary/accounts.yaml --env=contracts=ml_on_blockchain/smart-contracts/federatedLearning/ 1 deploy/diablo/primary/setup.yaml ml_on_blockchain/workload/federated_learning.yaml; ' &
     # wait 5 seconds that primary is launched and then launch secondary
     sleep 5
     ssh user@dclbigmem.epfl.ch -p 2232 "/home/user/diablo/diablo secondary -vvv localhost" &
