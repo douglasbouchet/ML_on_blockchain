cae6af16b321f1fa1eb4a0447fe7f8d972742ca4
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-01-26 16:42:48 +0000

    don't asking for host authenticity in setup-vm.sh

--- script/aws_script/setup-vm.sh
+++ script/aws_script/setup-vm.sh
@@ -53,15 +53,17 @@ timeout=5 # if the connection is not established within 5 seconds, exit with an 
 
 # copy smart contract folder to primary i.e should result in ubuntu@$1:contracts/learn_task. Also install required python packages to run arguments on primary
 echo "copying smart contract folder to primary node + installing required python packages"
-ssh ubuntu@$primary 'mkdir -p contracts/learn_task && pip3 install coincurve && pip3 install pysha3'
+# ssh-keyscan -H primary >> ~/.ssh/known_hosts
+# ssh ubuntu@$primary 'mkdir -p contracts/learn_task && pip3 install coincurve && pip3 install pysha3'
+ssh -o "StrictHostKeyChecking no" ubuntu@$primary 'mkdir -p contracts/learn_task && pip3 install coincurve && pip3 install pysha3'
 
 # if ! scp -o ConnectTimeout=$timeout ../../smart-contracts/federatedLearning/learn_task/arguments ubuntu@$1:~/contracts/learn_task; then
-if ! scp -o ConnectTimeout=$timeout ../../smart-contracts/federatedLearning/learn_task/arguments ubuntu@$primary:~/contracts/learn_task; then
+if ! scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout ../../smart-contracts/federatedLearning/learn_task/arguments ubuntu@$primary:~/contracts/learn_task; then
       #echo "Error: scp failed to connect within $timeout seconds. Verify that address: ubuntu@$1 is reachable."
       echo "Error: scp failed to connect within $timeout seconds. Verify that address: ubuntu@$primary is reachable."
       exit 1
 fi
-if ! scp -o ConnectTimeout=$timeout generated/contract.sol ubuntu@$primary:~/contracts/learn_task; then
+if ! scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout generated/contract.sol ubuntu@$primary:~/contracts/learn_task; then
       echo "Error: scp failed to connect within $timeout seconds. Verify that address: ubuntu@$primary is reachable."
       #echo "Error: scp failed to connect within $timeout seconds. Verify that address: ubuntu@$1 is reachable."
       exit 1
@@ -71,11 +73,11 @@ fi
 for var in $all_nodes;do # read the list of ip addresses
 
     # Use scp to copy the file to the remote IP address, and exit with an error message if nothing happens after 5 seconds
-    if ! scp -o ConnectTimeout=$timeout generated/workload.yaml ubuntu@$var:~; then
+    if ! scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout generated/workload.yaml ubuntu@$var:~; then
       echo "Error: scp failed to connect within $timeout seconds. Verify that address: ubuntu@$var is reachable."
       exit 1
     fi
-    if ! scp -o ConnectTimeout=$timeout generated/setup.yaml ubuntu@$var:~; then
+    if ! scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout generated/setup.yaml ubuntu@$var:~; then
       echo "Error: scp failed to connect within $timeout seconds. Verify that address: ubuntu@$var is reachable."
       exit 1
     fi
@@ -85,3 +87,5 @@ done
 # StrictHostKeyChecking=no
 
 # 13.38.8.6 13.38.121.116 13.39.14.186 35.180.42.83 13.38.227.66 35.180.30.38 35.180.43.190 13.39.111.237 35.180.29.49 35.180.190.213
+
+# 15.188.49.247 13.38.129.170 13.39.87.210 35.180.234.185 52.47.114.75 52.47.183.9 13.39.80.62 13.38.117.59
