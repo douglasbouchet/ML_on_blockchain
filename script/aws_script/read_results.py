01181fd58fd7e69f872c02ad127b5f1a462ba052
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-02-03 17:10:26 +0000

    rm unused scripts

--- script/aws_script/read_results.py
+++ script/aws_script/read_results.py
@@ -2,6 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import make_interp_spline
+import plotly.express as px
 
 
 # waiting_times = []
@@ -151,13 +152,12 @@ def plot_model_length_perf():
 
 
 def plot_varying_perf():
-    # model_length_range = [100_000]
-    # model_length_range = [300_000]
-    model_length_range = [1_000_000]
+    # model_length_range = [50_000, 300_000, 600_000, 1_000_000]
+    model_length_range = [50_000, 300_000]
     # workers_range = [5*2**i for i in range(10)]
     # workers_range = [5*2**i for i in range(9)]
     # workers_range = [5*2**i for i in range(8)]
-    workers_range = [5*2**i for i in range(2)]
+    workers_range = [2**i for i in range(10)]
     print("workers_range:", workers_range)
     n_runs = 2
     for model_length in model_length_range:
@@ -191,27 +191,48 @@ def plot_varying_perf():
             # we finished all runs for this worker number, so compute the mean and append it inside model_length_committed
             worker_mean = np.mean(all_perc_committed, axis=0)
             model_length_committed.append(worker_mean)
-            print("model_length_committed:", model_length_committed)
+            # print("model_length_committed:", model_length_committed)
         print("final model_length_committed:", model_length_committed)
-        fig = plt.figure()
-        plt.title(
-            "% commited txs by varying number of workers for model length {}".format(model_length))
-        plt.xlabel("Number of workers")
-        plt.ylabel("Commit %")
-        plt.plot(
-            workers_range,
-            model_length_committed,
-            linestyle="dashed",
-            marker="o",
-            label="mean over xx runs",
-        )
-        plt.xscale("log")
-        plt.grid(True)
-        plt.legend()
+        # fig = plt.figure()
+        # plt.title(
+        #     "% commited txs by varying number of workers for model length {}".format(model_length))
+        # plt.xlabel("Number of workers")
+        # plt.ylabel("Commit %")
+        # plt.plot(
+        #     workers_range,
+        #     model_length_committed,
+        #     linestyle="dashed",
+        #     marker="o",
+        #     label="mean over xx runs",
+        # )
+        # plt.ylim(-2, 100)
+        # plt.xscale("log")
+        # plt.grid(True)
+        # plt.legend()
+
+        print(len(workers_range), len(model_length_committed))
+        print("workers_range:", workers_range)
+        print("model_length_committed:", model_length_committed)
+        # flat model_length_committed
+        model_length_committed = [
+            item for sublist in model_length_committed for item in sublist]
+        print("model_length_committed:", model_length_committed)
+        img_path = "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length_plotly".format(
+            model_length)
+        fig = px.line(x=workers_range, y=model_length_committed, log_x=True, title="% commited txs by varying number of workers for model length {}".format(
+            model_length), labels={'x': "Number of workers", 'y': "Commit %"})
+        fig.update_traces(
+            mode="lines+markers", hovertemplate="Number of Workers: %{x}<br>Commit %: %{y}", name="xxx")
+        fig.update_layout(yaxis=dict(
+            range=[-2, 100], autorange=False), legend=dict(title="Legend", font=dict(size=14)), showlegend=True)
+
+        fig.write_image(
+            "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length_plotly.png".format(model_length))
+
         # save figure
-        print("saving path:", "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length.png".format(model_length))
-        plt.savefig(
-            "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length.png".format(model_length))
+        # print("saving path:", "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length.png".format(model_length))
+        # plt.savefig(
+        #     "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length.png".format(model_length))
 
 
 if __name__ == "__main__":
