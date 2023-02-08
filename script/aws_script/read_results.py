8c7a9943f0dbdaeacc25a9ea92245da4233bf614
Douglas Bouchet <douglas.bouchet@epfl.ch>
2023-02-08 10:06:07 +0000

    better figure for varying time

--- script/aws_script/read_results.py
+++ script/aws_script/read_results.py
@@ -7,95 +7,21 @@ import os
 from matplotlib.ticker import ScalarFormatter
 
 
-# waiting_times = []
-# percentages_committed = []
-# n_txs = []
-# scenario = "10k_model"
-# # workers = [10 * pow(10, i) for i in range(4)]
-# # workers = [10 * pow(2, i) for i in range(16)]
-# model_lengths = [10_000, 100_000, 1_600_000]
-
-# i = 0
-# # for n_workers in workers:
-# for model_length in model_lengths:
-#     i += 1
-#     # print("n_workers:", n_workers)
-#     # with open("res/{}/{}_workers.txt".format(scenario, n_workers)) as json_file:
-#     with open("res/{}/100_workers_70_redundancy_{}_model_length.txt".format(scenario, model_length)) as json_file:
-#         data = json.load(json_file)
-#         tx_submitted = 0
-#         tx_committed = 0
-#         tot_commit_time = 0
-#         for tx in data["Locations"][0]["Clients"][0]["Interactions"]:
-#             submit_time = tx["SubmitTime"]
-#             commit_time = tx["CommitTime"]
-#             tx_submitted += 1
-#             if commit_time != -1:
-#                 tx_committed += 1
-#                 tot_commit_time += commit_time - submit_time
-#         perc_committed = tx_committed / tx_submitted * 100
-#         avg_commit_time = tot_commit_time / tx_committed if tx_committed != 0 else 0
-#         print("tx_submitted:", tx_submitted)
-#         print("tx_committed:", tx_committed)
-#         print("avg_commit_time", avg_commit_time)
-#         print("perc_committed:", perc_committed)
-#         waiting_times.append(avg_commit_time)
-#         percentages_committed.append(perc_committed)
-#         # if tx_commited is bigger than 1000, replace thsousands with k
-#         if tx_committed > 1000:
-#             n_txs.append("{}k txs".format(tx_committed // 1000))
-#         else:
-#             n_txs.append("{} txs".format(tx_committed))
-
-# print("i:", i)
-# fig, axs = plt.subplots(2, 1, figsize=(15, 7.5))
-# # add vertical padding
-# fig.subplots_adjust(hspace=0.5)
-
-# axs[0].set_title("Average waiting time")
-# axs[1].set_title("Percentage committed")
-# fig.suptitle("Results")
-
-# #axs[0].plot(workers, waiting_times, linestyle="dashed", marker="o")
-# axs[0].plot(model_lengths, waiting_times, linestyle="dashed", marker="o")
-# axs[0].set_xlabel("Number of model_lengths")
-# axs[0].set_ylabel("Average waiting time (s)")
-# axs[1].plot(
-#     # workers,
-#     model_lengths,
-#     percentages_committed,
-#     linestyle="dashed",
-#     marker="o",
-# )
-# #axs[1].set_xlabel("Number of workers")
-# axs[1].set_xlabel("Number of model_lengths")
-# axs[1].set_ylabel("Percentage committed")
-# for i, ax in enumerate(axs):
-#     ax.grid(True)
-#     ax.set_xscale("log")
-#     for x, y, label in zip(
-#         # workers,
-#         model_lengths,
-#         waiting_times if i == 0 else percentages_committed,
-#         n_txs,
-#     ):
-#         ax.text(x, y, label, ha="center", va="bottom", fontsize=12, color="b")
-# # save figure
-# plt.savefig("res/{}/results.png".format(scenario))
-# plt.show()
-
-
-def plot_model_length_perf():
+def plot_max_array_length():
+    # model_length_range = np.arange(1000, 49000, 1000)
+    model_length_range = np.arange(
+        1000, 50000, 2000)  # select half of the points
+    # 1000, 49000, 2000)  # select half of the points
     all_perc_committed = []
-    for i in range(10):
+    #n_runs = 9
+    n_runs = 20
+    for run_nb in range(1, n_runs+1):
         waiting_times = []
         percentages_committed = []
         n_txs = []
-        # model_length_range = np.arange(1000, 51000, 1000)
-        model_length_range = np.arange(1000, 51000, 1000)
         for model_length in model_length_range:
-            with open("/home/user/ml_on_blockchain/results/max_model_size_{}/{}.txt".format(i, model_length)) as json_file:
-                # with open("/home/user/ml_on_blockchain/results/max_model_size/{}.txt".format(model_length)) as json_file:
+            print("run_nb:", run_nb, "model_length:", model_length)
+            with open("/home/user/ml_on_blockchain/results/aws/varying_array_length/run_{}/{}.txt".format(run_nb, model_length)) as json_file:
                 data = json.load(json_file)
                 tx_submitted = 0
                 tx_committed = 0
@@ -108,56 +34,79 @@ def plot_model_length_perf():
                         tx_committed += 1
                         tot_commit_time += commit_time - submit_time
                 perc_committed = tx_committed / tx_submitted * 100
-                avg_commit_time = tot_commit_time / tx_committed if tx_committed != 0 else 0
-                # print("tx_submitted:", tx_submitted)
-                # print("tx_committed:", tx_committed)
-                # print("avg_commit_time", avg_commit_time)
-                # print("i:", i, "model_length:", model_length,
-                #       "perc_committed:", perc_committed)
                 if (perc_committed == 0):
                     print("model_length:", model_length, "i:", i)
-                # print("perc_committed:", perc_committed)
-                waiting_times.append(avg_commit_time)
                 percentages_committed.append(perc_committed)
-        # print("percentage commited:", percentages_committed)
         all_perc_committed.append(percentages_committed)
-        # print("all_perc_committed:", all_perc_committed)
 
     mean_commited = np.mean(all_perc_committed, axis=0)
-    # print("mean:", mean_commited)
-    # TODO check if this is correct
-    # fig = plt.figure(figsize=(15, 7.5))
+    min_commited = mean_commited - np.min(all_perc_committed, axis=0)
+    max_commited = np.max(all_perc_committed, axis=0) - mean_commited
+
+    asymmetric_error = [min_commited, max_commited]
+    print("min_commited:", min_commited)
+    print("max_commited:", max_commited)
+
     fig = plt.figure()
-    plt.title("Percentage of committed transactions for different model lengths")
-    plt.xlabel("Model number of parameters (uint256)")
-    plt.ylabel("Commit %")
-    plt.plot(
-        # workers,
+    # set fig size
+    fig.set_size_inches(18.5, 10.5)
+    graph = plt.bar(
         model_length_range,
         mean_commited,
-        linestyle="dashed",
-        marker="o",
-        label="mean over 10 runs",
+        width=1000,
+        label="mean over 20 runs",
+        color='royalblue',
     )
-    smoothed = make_interp_spline(model_length_range, mean_commited)
-    X = np.linspace(model_length_range.min(),
-                    model_length_range.max(), 1000)
-    commited_smoothed = smoothed(X)
-    plt.plot(X, commited_smoothed, label="smoothed")
-    plt.grid(True)
-    # add legend
+    # add error bars
+    plt.errorbar(
+        model_length_range + 80,
+        mean_commited,
+        yerr=asymmetric_error,
+        fmt='none',
+        ecolor='black',
+        marker='o',
+        elinewidth=3,
+        capsize=6,
+        label="Range of values over 20 runs",
+    )
+    i = 0
+    for p in graph:
+        width = p.get_width()
+        height = p.get_height()
+        x, y = p.get_xy()
+
+        plt.text(x+width/2 - 450,
+                 y+height*1.01,
+                 str(int(mean_commited[i])),
+                 ha='center',
+                 weight='bold',
+                 fontsize=14)
+        i += 1
+    # plt.ylim(50, 100)
+    plt.ylim(40, 100)
+    fontsize = 18
+    plt.xticks(model_length_range, [str(int(x/1000)) + 'k'
+               for x in model_length_range])
+    plt.title("Percentage of committed transactions for different array size",
+              fontsize=fontsize+2)
+    plt.xlabel("Array size",
+               fontsize=fontsize)
+    plt.ylabel("Commit %", fontsize=fontsize)
+    plt.rcParams.update({'font.size': 14})
+    plt.xticks(fontsize=fontsize - 3)
+    plt.yticks(fontsize=fontsize)
+    # plt.grid(True)
     plt.legend()
 
-    # save figure
     plt.savefig(
-        "/home/user/ml_on_blockchain/results/images/max_model_length.png")
+        "/home/user/ml_on_blockchain/results/images/aws/max_array_length.png")
 
 
 def plot_varying_perf():
     # model_length_range = [50_000, 300_000, 600_000, 1_000_000]
     # model_length_range = [50_000, 100_000, 300_000, 600_000, 1_000_000]
-    model_length_range = [50_000, 100_000, 300_000, 600_000]
-    workers_range = [2**i for i in range(7)]
+    model_length_range = [50_000, 100_000, 300_000, 600_000, 1_000_000]
+    workers_range = [2**i for i in range(10)]
     print("workers_range:", workers_range)
     n_runs = 1
     model_perf = []
@@ -194,40 +143,24 @@ def plot_varying_perf():
             model_length_committed.append(worker_mean)
             # print("model_length_committed:", model_length_committed)
         print("final model_length_committed:", model_length_committed)
-        # fig = plt.figure()
-        # plt.title(
-        #     "% commited txs by varying number of workers for model length {}".format(model_length))
-        # plt.xlabel("Number of workers")
-        # plt.ylabel("Commit %")
-        # plt.plot(
-        #     workers_range,
-        #     model_length_committed,
-        #     linestyle="dashed",
-        #     marker="o",
-        #     label="mean over xx runs",
-        # )
-        # plt.ylim(-2, 100)
-        # plt.xscale("log")
-        # # save figure
-        # print("saving path:", "/home/user/ml_on_blockchain/results/images/aws/varying_time/{}_model_length.png".format(model_length))
-        # plt.savefig(
-        #     "/home/user/ml_on_blockchain/results/images/aws/varying_time/{}_model_length.png".format(model_length))
         model_perf.append(model_length_committed)
-    # fig = plt.figure()
+    fontsize = 18
     fig, ax = plt.subplots()
-    # inceased size
     fig.set_size_inches(12.5, 8)
     plt.title(
-        "% commited txs by varying number of workers for different model lengths")
-    plt.xlabel("Number of workers")
-    plt.ylabel("Commit %")
+        "% commited transactions by varying number of workers for different model lengths",
+        fontsize=fontsize)
+    plt.xlabel("Number of workers", fontsize=fontsize)
+    plt.ylabel("Commit %", fontsize=fontsize)
+    plt.xticks(fontsize=fontsize-2)
+    plt.yticks(fontsize=fontsize-2)
     plt.xscale("log")
     plt.ylim(-2, 100)
+    plt.grid(True)
     for i in range(len(model_perf)):
         plt.plot(
             workers_range,
             model_perf[i],
-            linestyle="dashed",
             marker="o",
             label="model length {}".format(model_length_range[i]),
         )
@@ -239,47 +172,23 @@ def plot_varying_perf():
         "Model length 600k ; Time: 600s",
         "Model length 1000k ; Time: 1000s"))
     props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-
-    # add a third measuremnet on the right
-    ax2 = ax.twinx()
-    ax2.set_ylabel("Time used for one learning step")
-    ax2.set_ylim(-2, 1000)
-    ax2.set_yticks([50, 100, 300, 600, 1000])
-    ax2.set_yticklabels(
-        ["50k: 50s", "100k: 100s", "300k: 300s", "600k: 600s", "1000k: 1000s"])
-    # display legend for line
     ax.legend(loc="lower left")
-    #  move to bottom left corner
-
-    # place a text box in upper left in axes coords
-    # ax.text(0.05, 0.7, textstr, transform=ax.transAxes, fontsize=14,
-    #         verticalalignment='top', bbox=props)
+    ax.set_xticks(workers_range)
+    ax.set_xticklabels(workers_range)
+    print("model_perf:", model_perf)
+    # flat modle_perf
+    flat_model_perf = [item for sublist in model_perf for item in sublist]
+    # [str(int(length / 1000)) + "k" for length in workers_range])
+    # ax.set_yticks(flat_model_perf)
+    # ax.set_yticklabels(flat_model_perf)
+    # [str(worker) for worker in max_n_workers])
     plt.savefig(
         "/home/user/ml_on_blockchain/results/images/aws/varying_time/all.png".format(model_length))
-
-    # print(len(workers_range), len(model_length_committed))
-    # print("workers_range:", workers_range)
-    # print("model_length_committed:", model_length_committed)
-    # # flat model_length_committed
-    # model_length_committed = [
-    #     item for sublist in model_length_committed for item in sublist]
-    # print("model_length_committed:", model_length_committed)
-    # img_path = "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length_plotly".format(
-    #     model_length)
-    # fig = px.line(x=workers_range, y=model_length_committed, log_x=True, title="% commited txs by varying number of workers for model length {}".format(
-    #     model_length), labels={'x': "Number of workers", 'y': "Commit %"})
-    # fig.update_traces(
-    #     mode="lines+markers", hovertemplate="Number of Workers: %{x}<br>Commit %: %{y}", name="xxx")
-    # fig.update_layout(yaxis=dict(
-    #     range=[-2, 100], autorange=False), legend=dict(title="Legend", font=dict(size=14)), showlegend=True)
-
-    # fig.write_image(
-    #     "/home/user/ml_on_blockchain/results/images/varying_worker/{}_model_length_plotly.png".format(model_length))
 
 
 def plot_varying_perf_constant_time():
     model_perf = []
-    model_length_range = [50_000, 100_000,
+    model_length_range = [50_000, 100_000, 200_000,
                           300_000, 600_000, 1_000_000, 5_000_000]
     workers_range = [2**(i) for i in range(9)]
     print("model_length_range:", model_length_range)
@@ -324,17 +233,6 @@ def plot_varying_perf_constant_time():
         # flat model_length_committed
         model_length_committed = [
             item for sublist in model_length_committed for item in sublist]
-        img_path = "/home/user/ml_on_blockchain/results/images/aws/constant_time/{}_model_length_plotly".format(
-            model_length)
-
-        fig = px.line(x=workers_range, y=model_length_committed, log_x=True, title="% commited txs by varying number of workers for model length {}".format(
-            model_length), labels={'x': "Number of workers", 'y': "Commit %"})
-        fig.update_traces(
-            mode="lines+markers", hovertemplate="Number of Workers: %{x}<br>Commit %: %{y}", name="xxx")
-        fig.update_layout(yaxis=dict(
-            range=[-2, 100], autorange=False), legend=dict(title="Legend", font=dict(size=14)), showlegend=True)
-
-        fig.write_image(img_path + ".png")
     fig, ax = plt.subplots()
     fig.set_size_inches(12.5, 8)
     fontsize = 18
@@ -367,9 +265,9 @@ def plot_varying_perf_constant_time():
 
 
 def plot_max_n_workers_as_model_length():
-    model_length_range = [50_000, 100_000,
+    model_length_range = [50_000, 100_000, 200_000,
                           300_000, 600_000, 1_000_000, 5_000_000]
-    max_n_workers = [64, 32, 8, 4, 4, 0]
+    max_n_workers = [64, 32, 16, 8, 4, 4, 0]
     fig, ax = plt.subplots()
     fig.set_size_inches(12.5, 8)
     fontsize = 18
@@ -406,7 +304,7 @@ def plot_max_n_workers_as_model_length():
 
 
 if __name__ == "__main__":
-    # plot_model_length_perf()
-    # plot_varying_perf()
+    # plot_max_array_length()
+    plot_varying_perf()
     # plot_varying_perf_constant_time()
-    plot_max_n_workers_as_model_length()
+    # plot_max_n_workers_as_model_length()
